7cc12b90a8743add4ace96a2a08036affb7f4d8d
Jackson Walters <jacksonwalters@gmail.com>
2024-12-12 20:20:47 -0500

    convert polynomials to Vec<i64> of coefficients
    still have an issue with truncating secret key

--- module-lwe/src/main.rs
+++ module-lwe/src/main.rs
@@ -32,29 +32,33 @@ fn main() {
         let (a,t,sk) = keygen(n,q as i64,k,&f);
         let pk = (a,t);
 
-        // Convert public and secret keys to lists of coefficients
-        let mut pk_coeffs: Vec<i64> = pk.0.iter() 
-            .flat_map(|row| row.iter().flat_map(|poly| poly.coeffs()))
-            .cloned()
+        println!("{:?}",sk);
+
+        // Convert public key to a flattened list of coefficients
+        let mut pk_coeffs: Vec<i64> = pk.0
+            .iter()
+            .flat_map(|row| row.iter().flat_map(|poly| poly.coeffs().to_vec()))
             .collect();
-        pk_coeffs.extend(pk.1.iter().flat_map(|poly| poly.coeffs()).cloned()); 
+        pk_coeffs.extend(pk.1.iter().flat_map(|poly| poly.coeffs().to_vec()));
 
-        let sk_coeffs: Vec<i64> = sk.iter().flat_map(|poly| poly.coeffs()).cloned().collect();
+        // Convert secret key to a flattened list of coefficients
+        let sk_coeffs: Vec<i64> = sk
+            .iter()
+            .flat_map(|poly| poly.coeffs().to_vec())
+            .collect();
 
-        // Convert the public key coefficients to a comma-separated string
+        // Convert the public/secret key coefficients to a comma-separated string
         let pk_coeffs_str = pk_coeffs.iter()
             .map(|coef| coef.to_string())
             .collect::<Vec<String>>()
             .join(",");
-    
-            // Convert the secret key coefficients to a comma-separated string
         let sk_coeffs_str = sk_coeffs.iter()
             .map(|coef| coef.to_string())
             .collect::<Vec<String>>()
             .join(",");
-
+        
+        //store the secret/public key in a HashMap
         let mut keys: HashMap<String, String> = HashMap::new();
-
         keys.insert(String::from("secret"), sk_coeffs_str);
         keys.insert(String::from("public"), pk_coeffs_str);
 
