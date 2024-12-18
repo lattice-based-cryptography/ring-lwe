a0de33d5e3c23e9c079955a9bbced1ddebdaf07e
Jackson Walters <jacksonwalters@gmail.com>
2024-12-18 14:10:42 -0500

    use consistent parameters names

--- ring-lwe/src/keygen.rs
+++ ring-lwe/src/keygen.rs
@@ -2,11 +2,11 @@ use polynomial_ring::Polynomial;
 use ring_lwe::{Parameters, polymul, polyadd, gen_binary_poly, gen_uniform_poly, gen_normal_poly};
 use std::collections::HashMap;
 
-pub fn keygen(size: usize, modulus: i64, f: &Polynomial<i64>) -> ([Polynomial<i64>; 2], Polynomial<i64>) {
+pub fn keygen(n: usize, modulus: i64, f: &Polynomial<i64>) -> ([Polynomial<i64>; 2], Polynomial<i64>) {
     // Generate a public and secret key
-    let sk = gen_binary_poly(size);
-    let a = gen_uniform_poly(size, modulus);
-    let e = gen_normal_poly(size);
+    let sk = gen_binary_poly(n);
+    let a = gen_uniform_poly(n, modulus);
+    let e = gen_normal_poly(n);
     let b = polyadd(&polymul(&-&a, &sk, modulus, &f), &-&e, modulus, &f);
     
     // Return public key (b, a) as an array and secret key (sk)
